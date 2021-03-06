3410c35c4ba83e4b517fc8c2e73782e2bc3b1446
Gineth <ginysp@hotmail.com>
2021-03-06 04:59:13 +0000

    include

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-window.onload(
+window.onload= function() {
     document.getElementById("mensajeerror").style.display= "none"
-);
+}
 
 let pagoform = document.getElementById("PagoForm");
 
@@ -16,14 +16,125 @@ let mastercard = document.getElementById("mastercard");
 let visa = document.getElementById("visa");
 let diners = document.getElementById("diners");
 let amex = document.getElementById("amex");
+let mensaje = document.getElementById("mensaje");
 
 pagoform.addEventListener("submit", function(e){
     e.preventDefault();
 
-    if(tj.value == '' || cvc.value=='')
+    if(tj.value == '')
     {
-         document.getElementById("mensajeerror").style.display= "block"
-         tj.style.background = "red";
+		document.getElementById("mensajeerror").style.display= "block"
+        tj.style.background = "#F5B7B1";
     }
+	else
+	{
+		document.getElementById("mensajeerror").style.display= "none"
+		tj.style.background = "#FFFFFF";
+	}
+	
+	if(cvc.value=='')
+    {
+        document.getElementById("mensajeerror").style.display= "block"
+		cvc.style.background = "#F5B7B1";
+    }
+	else
+	{
+		document.getElementById("mensajeerror").style.display= "none"
+		cvc.style.background = "#FFFFFF";
+	}
+	
+	if(amount.value=='')
+    {
+        document.getElementById("mensajeerror").style.display= "block"
+		amount.style.background = "#F5B7B1";
+    }
+	else
+	{
+		document.getElementById("mensajeerror").style.display= "none"
+		amount.style.background = "#FFFFFF";
+	}
+	
+	if(name.value=='')
+    {
+        document.getElementById("mensajeerror").style.display= "block"
+		name.style.background = "#F5B7B1";
+    }
+	else
+	{
+		document.getElementById("mensajeerror").style.display= "none"
+		name.style.background = "#FFFFFF";
+	}
+	
+	if(lastname.value=='')
+    {
+        document.getElementById("mensajeerror").style.display= "block"
+		lastname.style.background = "#F5B7B1";
+    }
+	else
+	{
+		document.getElementById("mensajeerror").style.display= "none"
+		lastname.style.background = "#FFFFFF";
+	}
+	
+	if(city.value=='')
+    {
+        document.getElementById("mensajeerror").style.display= "block"
+		city.style.background = "#F5B7B1";
+    }
+	else
+	{
+		document.getElementById("mensajeerror").style.display= "none"
+		city.style.background = "#FFFFFF";
+	}
+	
+	if(postalcode.value=='')
+    {
+        document.getElementById("mensajeerror").style.display= "block"
+		postalcode.style.background = "#F5B7B1";
+    }
+	else
+	{
+		document.getElementById("mensajeerror").style.display= "none"
+		postalcode.style.background = "#FFFFFF";
+	}
+	
+	if(state.value == "-1")
+	{
+		document.getElementById("mensajeerror").style.display= "block"
+		state.style.background = "#F5B7B1";
+	}
+	else
+	{
+		document.getElementById("mensajeerror").style.display= "none"
+		state.style.background = "#FFFFFF";
+	}
+	
+	if(!amex.checked && !mastercard.checked && !visa.checked && !diners.checked)
+	{
+		document.getElementById("mensajeerror").style.display= "block"
+		amex.style.background = "#F5B7B1";
+		mastercard.style.background = "#F5B7B1";
+		visa.style.background = "#F5B7B1";
+		diners.style.background = "#F5B7B1";
+	}
+	else
+	{
+		amex.style.background = "#FFFFFF";
+		mastercard.style.background = "#FFFFFF";
+		visa.style.background = "#FFFFFF";
+		diners.style.background = "#FFFFFF";
+		document.getElementById("mensajeerror").style.display= "none"
+	}
+	
+	if(mensaje.value == "")
+	{
+		document.getElementById("mensajeerror").style.display= "block"
+		mensaje.style.background = "#F5B7B1";
+	}
+	else
+	{	
+		document.getElementById("mensajeerror").style.display= "none"
+		mensaje.style.background = "#FFFFFF";
+	}
 
 });
